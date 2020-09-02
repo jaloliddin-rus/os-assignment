86cdcf429a5b1f5ea428480c448428a6e59c2485
Jaloliddin Rustamov <38163017+seamonkeyhd@users.noreply.github.com>
2020-09-03 01:54:21 +0800

    Add files via upload

---
+++ Login.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+echo "Login Screen"
+echo "=========================="
+echo "Email: "
+read lemail
+echo "Password: "
+read -s lpassword
+INPUT="login.txt"
+OLDIFS=$IFS
+IFS=,
+[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+while read -r email password dept pos
+do
+if [[ $lemail = $email ]]
+	then
+		if [[ $lpassword = $password ]]
+			then
+				echo "login successful"
+				echo "$email works in $dept and he or she is a $pos"
+				break
+			else
+				echo "login failed"
+		fi
+fi
+done < $INPUT
+echo "user does not exist"
+IFS=$OLDIFS
+
+
