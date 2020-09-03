92ff02373af5a784d79147f9c3fe90bab60a9eb2
seamonkeyhd <mr.romaboy@gmail.com>
2020-09-03 15:57:55 -0700

    almost done

--- EmpPerfReviewForm.sh
+++ EmpPerfReviewForm.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 #author: Jaloliddin
-echo "U MADE IT"
+kpiDB="KPI.txt"
+echo "Employee Performance Review Form"
+echo "------------------------"
+echo -n "Please enter the KPI Code: "
+read kpi
+
+while IFS=: read -r kpiCode kpiCriteria kpiDesc; do
+	if [[ $kpi == $kpiCode ]]; then
+		echo "KPI - Key Performance Indicator: $kpiCriteria"
+	fi
+done <$kpiDB
+
+echo -n "Plese enter the Rate obtained (max 10 - min 0): "
+read rate
+
+echo "Comments (if any): "
+read comment
+
+echo -n "Press (y) to continue to the Employee's marks or (b) to return to the previous screen: "
+read choice
+case $choice in
+	y) ./EmpPerfReviewForm.sh ;;
+	b) ./EmpValidationForm.sh ;;
+	*) echo -n "Invalid Choice!" ;;
+esac
