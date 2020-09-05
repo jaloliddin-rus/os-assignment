e66fa65595ded5520a726e0802ff9c9d93eb03dc
Simran Nijhar <simran.nijhar@gmail.com>
2020-09-05 17:36:09 +0800

    Add new emp

---
+++ AddNewEmpForm.sh
@@ -0,0 +1,21 @@
+A#!/bin/bash
+#Author: Harrsimran Kaur
+echo "Add New Employee Form"
+echo "====================="
+echo
+echo -n "Employee IC. Number : "
+read empICNo
+echo -n "Employee Name : "
+read empName
+echo -n "Contact Number : "
+read empContactNo
+echo -n "Email : "
+read empEmail
+echo -n "Gender : "
+read empGender
+echo -n "Birth Date : "
+read empBirthDate
+echo -n "Job Title : "
+read empJobTitle
+echo -n "Joined Date : "
+read empJoinedDate
