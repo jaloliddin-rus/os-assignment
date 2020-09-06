8eda806351a4f860d4d982644a3e522f4b352db6
seamonkeyhd <mr.romaboy@gmail.com>
2020-09-05 19:49:51 -0700

    date validation added

--- EmpPerfReviewForm.sh
+++ EmpPerfReviewForm.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 #author: Jaloliddin
-kpiDB="KPI.txt"
+
+#variables passed down from another script
 ICNo=$1
 empName=$2
 startDate=$3
 endDate=$4
+
+#files
+kpiDB="KPI.txt"
+filename="${ICNo}KPIResult"
+echo $filename
+
 echo "Employee Performance Review Form"
 echo "------------------------"
 echo -n "Please enter the KPI Code: "
 read kpi
 
+#loop to retrieve the kpi criteria based on kpi code
 while IFS=: read -r kpiCode kpiCriteria kpiDesc; do
     if [[ $kpi == $kpiCode ]]; then
         echo "KPI - Key Performance Indicator: $kpiCriteria"
-    else
-        exec EmpPerfReviewForm.sh
     fi
 done <$kpiDB
 
 echo -n "Plese enter the Rate obtained (max 10 - min 0): "
 read rate
 
-echo "Comments (if any): "
+echo -n "Comments (if any): "
 read comment
 
 echo -n "Press (y) to continue to the Employee's marks or (b) to return to the previous screen: "
--- EmpValidationForm.sh
+++ EmpValidationForm.sh
@@ -1,13 +1,42 @@
 #!/bin/bash
 #Author: Jaloliddin
+
+empDB="Employee.txt"
+
+#constants
+declare -r TRUE=0
+declare -r FALSE=1
+
+isValidDate() {
+	local date="$1"
+	date "+%m-%Y" -d "$date" >/dev/null 2>&1
+	if [ $? != 0 ]; then
+		#1 = false
+		echo $FALSE
+	else
+		#0 = true
+		echo $TRUE
+	fi
+}
+
 echo "Employee Validation Form"
 echo "------------------------"
-echo -n "Please enter the Review Period (From mm-yyyy To mm-yyyy): "
-read startDate
-read endDate
+test1=$FALSE
+test2=$FALSE
+while [[ $test1 -eq 1 || $test2 -eq 1 ]]; do
+	echo -n "Please enter the Review Period (From mm-yyyy To mm-yyyy): "
+	read startDate
+	test1=$(isValidDate "$startDate")
+	read endDate
+	test2=$(isValidDate "$endDate")
+
+	if [[ $test1 -eq 1 || $test2 -eq 1 ]]; then
+		echo "Invalid Date!"
+	fi
+done
+
 echo -n "Plese enter the Employee's IC. Number (xxxxxx-xx-xxxx): "
 read ICNo
-empDB="Employee.txt"
 
 while IFS=: read -r empDept empICNo empName empPhone empEmail empGender empBirthDate empJobTitle empJoinDate; do
 	if [[ $empICNo == $ICNo ]]; then
@@ -21,7 +50,7 @@ done <$empDB
 echo -n "Press (n) to continue to the Employee Performance Review Form or (q) to quit from the prompt and return to Human Resources Management Menu: "
 read choice
 case $choice in
-	n) ./EmpPerfReviewForm.sh "$ICNo" "$name" "$startDate" "$endDate" ;;
-	q) ./HRMenu.sh ;;
-	*) echo -n "Invalid Choice!" ;;
+n) ./EmpPerfReviewForm.sh "$ICNo" "$name" "$startDate" "$endDate" ;;
+q) ./HRMenu.sh ;;
+*) echo -n "Invalid Choice!" ;;
 esac
