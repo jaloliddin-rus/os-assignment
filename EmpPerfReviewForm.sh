b49dfbc770756a3a980bc51c648eefc0855f22e6
seamonkeyhd <mr.romaboy@gmail.com>
2020-09-06 00:30:51 -0700

    almost done !

--- EmpPerfReviewForm.sh
+++ EmpPerfReviewForm.sh
@@ -10,30 +10,55 @@ endDate=$4
 #files
 kpiDB="KPI.txt"
 filename="${ICNo}KPIResult"
-echo $filename
 
-echo "Employee Performance Review Form"
-echo "------------------------"
-echo -n "Please enter the KPI Code: "
-read kpi
+#variables
+loop=y
+average=0.0
 
-#loop to retrieve the kpi criteria based on kpi code
-while IFS=: read -r kpiCode kpiCriteria kpiDesc; do
-    if [[ $kpi == $kpiCode ]]; then
-        echo "KPI - Key Performance Indicator: $kpiCriteria"
-    fi
-done <$kpiDB
+function calcAverage() {
+    
+}
 
-echo -n "Plese enter the Rate obtained (max 10 - min 0): "
-read rate
+printf '%-50sEmployee Performance Review\n' >>$filename.txt
+printf '%-50s===========================\n\n' >>$filename.txt
+printf 'Employee IC. Number: %s\n' "$ICNo" >>$filename.txt
+printf 'Employee Name: %s\n' "$empName" >>$filename.txt
+printf 'Review Period: From %s To %s \n' "$startDate" "$endDate" >>$filename.txt
+printf '%s========================================================================\n' >>$filename.txt
+printf '%-30s %-30s %-30s\n' "KPI Criteria:" "Rate Obtained:" "Comments:" >>$filename.txt
+printf '%s========================================================================\n' >>$filename.txt
 
-echo -n "Comments (if any): "
-read comment
+while [ "$loop" = "y" ]; do
+    echo "Employee Performance Review Form"
+    echo "------------------------"
+    echo -n "Please enter the KPI Code: "
+    read kpi
 
-echo -n "Press (y) to continue to the Employee's marks or (b) to return to the previous screen: "
-read choice
-case $choice in
-y) ./EmpPerfReviewForm.sh ;;
+    #loop to retrieve the kpi criteria based on kpi code
+    while IFS=: read -r kpiCode kpiCriteria kpiDesc; do
+        if [[ $kpi == $kpiCode ]]; then
+            echo "KPI - Key Performance Indicator: $kpiCriteria"
+            printCriteria=$kpiCriteria
+        fi
+    done <$kpiDB
+
+    echo -n "Plese enter the Rate obtained (max 10 - min 0): "
+    read rate
+
+    echo -n "Comments (if any): "
+    read comment
+
+    printf '%-30s %-30s %-30s\n' "$printCriteria" "$rate" "$comment" >>$filename.txt
+
+    echo -n "Press (y) to continue to the Employee's marks or (b) to return to the previous screen: "
+    read loop
+done
+
+case $loop in
 b) ./EmpValidationForm.sh ;;
 *) echo -n "Invalid Choice!" ;;
 esac
+
+if [[ $loop = b ]]; then
+
+fi
